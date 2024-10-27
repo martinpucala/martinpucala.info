28743706e43fc7bb95c24329bb1a61ef953254c0
Martin Púčala <martin.pucala@gmail.com>
2024-10-27 04:55:03 +0100

    update paddr ux slightly

--- paddrr/main.js
+++ paddrr/main.js
@@ -1,24 +1,26 @@
-$(function() {
-    var canvas = document.querySelector('#preview'),
-        defaultRatioWidth = 5,
-        defaultRatioHeight = 4;
+document.addEventListener('DOMContentLoaded', function() {
+    const canvas = document.querySelector('#preview');
+    const photoInput = document.querySelector('#photo');
+
+    let imageFile = null;
     
-    document.querySelector('#photo').onchange = function(e) {
-        var file = e.target.files[0];
-        var reader = new FileReader();
+    function doThings(e) {
+        imageFile = [...photoInput.files][0] || imageFile;
+
+        const reader = new FileReader();
         
-        reader.onload = function(e) {
+        reader.addEventListener('load', function(e) {
             var img = new Image();
-            img.onload = function() {
+            img.addEventListener('load', function() {
                 var q,
-                    w = +$('#ratio').val().split(':')[0],
-                    h = +$('#ratio').val().split(':')[1];
+                    w = +document.querySelector('#ratio').value.split(':')[0],
+                    h = +document.querySelector('#ratio').value.split(':')[1];
                 
                 if (isNaN(w)) {
-                    w = 4;
+                    w = defaultRatioWidth;
                 }
                 if (isNaN(h)) {
-                    h = 5;
+                    h = defaultRatioHeight;
                 }
                 
                 q = img.height / h;
@@ -27,16 +29,20 @@ $(function() {
                 
                 var ctx = canvas.getContext('2d');
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.fillStyle = $('#fill').val();
+                ctx.fillStyle = document.querySelector('#fill').value;
                 ctx.fillRect(0, 0, canvas.width, canvas.height);
                 ctx.drawImage(img, (canvas.width - img.width) / 2, 0);
                 
                 document.querySelector('#output').src = canvas.toDataURL('image/jpeg', 1.0);
                 document.querySelector('#output').className = 'loaded';
-            };
+            });
             img.src = e.target.result;
-        };
+        });
         
-        reader.readAsDataURL(file);
-    };
+        reader.readAsDataURL(imageFile);
+    }
+
+    photoInput.addEventListener('change', doThings);
+    document.querySelector('#ratio').addEventListener('change', doThings);
+    document.querySelector('#fill').addEventListener('change', doThings);
 });
